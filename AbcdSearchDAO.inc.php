347a9a561b54bf47be8daa49ce2c984c9996d3ea
Daniel Silva Ferreira <danielsilva@usp.br>
2023-11-13 10:05:20 -0300

    force convert utf-8

--- AbcdSearchDAO.inc.php
+++ AbcdSearchDAO.inc.php
@@ -10,27 +10,33 @@ class AbcdSearchDAO extends DAO {
         parent::__construct();
     }
 
+    // Método com função, que deve ser resgatado e passado ao arquivo .tpl via handler.inc.php
     public function obterDados() {
         $result = $this->retrieve(
-            'SELECT DISTINCT setting_value FROM publication_settings WHERE setting_name = ?', 
+            // DISTINCT obtem a lista de copyrights evitando repetição
+            'SELECT DISTINCT CONVERT(setting_value USING utf8) as setting_value FROM publication_settings WHERE CONVERT(setting_name USING utf8) = ?',
             ['copyrightHolder']
         );
 
+        // Inicializa um array para armazenar os resultados
         $dados = array();
 
+        // Percorre os resultados
         foreach ($result as $row) {
             $settingValue = $row->setting_value; 
             // Remove "Universidade de São Paulo. " da string
             $settingValue = str_replace("Universidade de São Paulo. ", "", $settingValue);
-
-            // Converta para UTF-8
-            $settingValue = mb_convert_encoding($settingValue, 'UTF-8', 'auto');
 
             $dados[] = $settingValue;
         }
 
         // Ordenar os resultados em ordem alfabética
         sort($dados);
+
+        // Converta para UTF-8
+        $dados = array_map(function($value) {
+            return mb_convert_encoding($value, 'UTF-8', 'auto');
+        }, $dados);
 
         return $dados;
     }
